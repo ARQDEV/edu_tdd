da402f1ba8fdc117e4f8aee3e56a44a2b1e840cc
skatalites <theskatalites@gmail.com>
2020-06-13 10:26:19 -0500

    Cambios con el calculo del IVA

---
+++ Cart/src/test/java/com/edu/dev/produtos/CalcularIvaProducto.java
@@ -0,0 +1,21 @@
+package com.edu.dev.produtos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcularIvaProducto {
+
+	@Test
+	void calcularIvaProducto() {
+
+		Producto tenis = new Producto();
+		tenis.setCodigo("T123");
+		tenis.setPrecio(150000D);
+		
+		
+		
+		//assertEquals(150000, tenis.getPrecio()+ tenis.IVA19);
+	}
+
+}
