fa34a56d67fccaf77df79a2e904d8b8e9847ed9a
cacu100 <cacu100@gmail.com>
2020-06-20 10:53:20 -0500

    Carlos proposal

--- Cart/src/main/java/com/edu/dev/calculadoriva/CalculadorIva.java
+++ Cart/src/main/java/com/edu/dev/calculadoriva/CalculadorIva.java
@@ -1,7 +1,9 @@
 package com.edu.dev.calculadoriva;
 
+import java.io.IOException;
+
 import com.edu.dev.productos.Producto;
 
 public interface CalculadorIva {
-    double getIvaProducto(Producto producto);
+    double getIvaProducto(Producto producto) throws IOException;
 }
--- Cart/src/main/java/com/edu/dev/calculadoriva/CalculadorIva19.java
+++ Cart/src/main/java/com/edu/dev/calculadoriva/CalculadorIva19.java
@@ -1,13 +1,47 @@
 package com.edu.dev.calculadoriva;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.edu.dev.productos.Producto;
 
 public class CalculadorIva19 implements CalculadorIva {
 
-    private static final double IVA_19 = 1.19;
+	//private static final double IVA_19 = 1.19;
 
-    @Override
-    public double getIvaProducto(Producto producto) {
-        return producto.getPrecio() * IVA_19;
-    }
+	
+
+	@Override
+	public double getIvaProducto(Producto producto) throws IOException {
+		
+		Properties properties = readPropertiesFile("config.properties");
+		double IVA = Double.parseDouble(properties.getProperty("iva"));
+		return producto.getPrecio() * IVA;
+	}
+
+	public static Properties readPropertiesFile(String fileName) throws IOException {
+		FileInputStream fis = null;
+		Properties prop = null;
+		try {
+			fis = new FileInputStream(fileName);
+			// create Properties class object
+			prop = new Properties();
+			// load properties file into it
+			prop.load(fis);
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} finally {
+			fis.close();
+		}
+
+		return prop;
+	}
+
 }
